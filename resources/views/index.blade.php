b5f642678e44b8197cc2a06ca7baaf74c1c56c72
Robin Panthee <robinpanthee@gmail.com>
2022-01-08 18:28:12 +0545

    small change mf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,10 +5,10 @@
     <div class="jumbotron">
         <h1>Forum App</h1>
         <p class="lead">
-            This is a simple web application developed with Laravel PHP Framework. <br>
+            This HELLO MFgit is a simple web application developed with Laravel PHP Framework. <br>
             It is made for educational purpose at <a href="https://daredevops.com">Dare DevOps</a>
         </p>
-    <a class="btn btn-lg btn-primary" href="{{route('posts.index')}}" role="button">Browse Posts &raquo;</a>
+        <a class="btn btn-lg btn-primary" href="{{route('posts.index')}}" role="button">Browse Posts &raquo;</a>
     </div>
 </div>
 @endsection
