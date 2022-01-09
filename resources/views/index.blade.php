29ed4922b432c8cd85a1d47644b380094d08ed25
Robin Panthee <robinpanthee@gmail.com>
2022-01-10 01:14:37 +0545

    done fobhayor t0oday00 OKo  ok

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="jumbotron">
         <h1>Forum App</h1>
         <p class="lead">
-            ASHU, Sleep Well darling. Good Night :) HAPPY HAPPY <br>
+            ASHU, Sleep Well darling. Good Night :) HAPPY HAPPY DHAYNYABAD<br>
             It is made for educational purpose at <a href="https://daredevops.com">Dare DevOps</a>
         </p>
         <a class="btn btn-lg btn-primary" href="{{route('posts.index')}}" role="button">Browse Posts &raquo;</a>
