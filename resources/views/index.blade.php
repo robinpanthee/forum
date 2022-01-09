5d9cd1231c79d1ed55cac33edea4f92926fc14ac
Robin Panthee <robinpanthee@gmail.com>
2022-01-10 01:00:07 +0545

    done for today

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,7 @@
     <div class="jumbotron">
         <h1>Forum App</h1>
         <p class="lead">
-            ASHMITA GRACE MY PROJECT:: IT MUST WORK NOW. IF IT WONT WORK NOW, I WILL STAY ALL NIGHT :( ) HOW ARE YOU a
-            simple web application developed with Laravel PHP Framework. <br>
+            ASHU, Sleep Well darling. Good Night :) <br>
             It is made for educational purpose at <a href="https://daredevops.com">Dare DevOps</a>
         </p>
         <a class="btn btn-lg btn-primary" href="{{route('posts.index')}}" role="button">Browse Posts &raquo;</a>
